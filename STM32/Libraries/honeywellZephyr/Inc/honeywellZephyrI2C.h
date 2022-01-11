49769095400dcc0e9762ad0e59145e23b924a90e
Anders Gnistrup <anders.gnistrup@copenhagenatomics.com>
2022-01-11 13:00:36 +0100

    Issue #90, Use pointer to I2C context.
    the code worked by some magic/luck. Now an initialized I2C context
    is required.

--- STM32/Libraries/honeywellZephyr/Inc/honeywellZephyrI2C.h
+++ STM32/Libraries/honeywellZephyr/Inc/honeywellZephyrI2C.h
@@ -1,37 +1,13 @@
+#pragma once
+
 /*
- * honeywellZephyrI2C.h
- *
- *  Created on: Mar 22, 2021
- *      Author: alema
+ * Description:
+ * Provides an interface to communicate with the honeywell zephyr air flow sensor.
  */
-
-/*
-Library:			STM32F411CE I2C honeywell zephyr
-Written by:			Alexander Mizrahi-Werner (alexanderMizrahi@copenhagenAtomics.com)
-Last modified:		22-03-2021
-Description:
-					Provides an interface to communicate with the honeywell zephyr air flow
-					sensor.	Tested with both STM32F103 and STM32F411CE
-
-
-
-*/
-
-#ifndef INC_HONEYWELLZEPHYRI2C_H_
-#define INC_HONEYWELLZEPHYRI2C_H_
-
-
-
-#endif /* INC_HONEYWELLZEPHYRI2C_H_ */
-
-
-
 #include "stm32f4xx_hal.h"
 
-//initialize sensor and get sensor serial number
-HAL_StatusTypeDef flowSensorInit(uint32_t *serialNB);
+// Get sensor serial number
+HAL_StatusTypeDef honeywellZephyrSerial(I2C_HandleTypeDef* hi2c, uint32_t *serialNB);
 
-//read sensor data
-HAL_StatusTypeDef ZephyrRead(float *flowData, uint8_t SLPM);
-
-void flowLED(float flowData);
+// read measured value.
+HAL_StatusTypeDef honeywellZephyrRead(I2C_HandleTypeDef *hi2c, float *flowData);
--- STM32/Libraries/honeywellZephyr/Src/honeywellZephyrI2C.c
+++ STM32/Libraries/honeywellZephyr/Src/honeywellZephyrI2C.c
@@ -1,72 +1,57 @@
+/* Description:
+ * Provides an interface to communicate with the honeywell zephyr air flow sensor. */
 
-/*
-Library:			STM32F411CE I2C honeywell zephyr
-Written by:			Alexander Mizrahi-Werner (alexanderMizrahi@copenhagenAtomics.com)
-Last modified:		22-03-2021
-Description:
-					Provides an interface to communicate with the honeywell zephyr air flow
-					sensor.	Tested with both STM32F103 and STM32F411CE
-
-
-
-*/
-#include "honeywellZephyrI2C.h"
 #include "stm32f4xx_hal.h"
-#include "string.h"
+#include "honeywellZephyrI2C.h"
 
 #define ZephyrADDR 0x49
-
-I2C_HandleTypeDef hi2c1;
-
-HAL_StatusTypeDef ZephyrRead(float *flowData, uint8_t SLPM)
+HAL_StatusTypeDef honeywellZephyrRead(I2C_HandleTypeDef *hi2c, float *flowData)
 {
     uint8_t readSensorADDR = 0x00;
 
     uint8_t addata[2];
-	int32_t placeHolder;
-	HAL_StatusTypeDef ret;
-	ret = HAL_I2C_Master_Transmit(&hi2c1, (uint16_t)(ZephyrADDR << 1), &readSensorADDR, 2, 50);
-	if(ret != HAL_OK){
-		return ret;
-	}
+    HAL_StatusTypeDef ret;
+    ret = HAL_I2C_Master_Transmit(hi2c, (uint16_t) (ZephyrADDR << 1), &readSensorADDR, 2, 50);
+    if (ret != HAL_OK)
+        return ret;
 
-	ret = HAL_I2C_Master_Receive(&hi2c1, (uint16_t)(ZephyrADDR << 1)|0x01, addata, 2, 50);
-	if(ret != HAL_OK){
-		return ret;
-	}
+    ret = HAL_I2C_Master_Receive(hi2c, (uint16_t) (ZephyrADDR << 1) | 0x01, addata, 2, 50);
+    if (ret != HAL_OK)
+        return ret;
 
-	placeHolder = ((uint16_t)addata[0]<< 8) | addata[1];
+    int32_t placeHolder = ((uint16_t) addata[0] << 8) | addata[1];
+    *flowData = ((placeHolder / 16384.0) - 0.1) / 0.8;
 
-    *flowData = SLPM * ((placeHolder/16384.0) - 0.1)/0.8;
-
-	return HAL_OK;
+    return HAL_OK;
 }
 
-HAL_StatusTypeDef flowSensorInit(uint32_t *serialNB){
-/*
- * The sensor prints out 2 bytes on startup with its serial number
- *
- */
+HAL_StatusTypeDef honeywellZephyrSerial(I2C_HandleTypeDef *hi2c, uint32_t *serialNB)
+{
     uint8_t readSerialNBADDR = 0x01;
+    *serialNB = 0;
 
-	uint8_t addata1[4];
-	HAL_StatusTypeDef ret;
-	ret = HAL_I2C_Master_Transmit(&hi2c1, (uint16_t)(ZephyrADDR << 1), &readSerialNBADDR, 1, 50);
-	if(ret != HAL_OK){
-		return ret;
-	}
-	HAL_Delay(2);
-	ret = HAL_I2C_Master_Receive(&hi2c1, (uint16_t)(ZephyrADDR << 1)|0x01, &addata1[0], 2, 50);
-	if(ret != HAL_OK){
-		return ret;
-	}
+    // The sensor prints out 2 bytes on startup with its serial number
+    uint8_t addata1[4];
+    HAL_StatusTypeDef ret;
+    ret = HAL_I2C_Master_Transmit(hi2c, (uint16_t) (ZephyrADDR << 1),
+            &readSerialNBADDR, 1, 50);
+    if (ret != HAL_OK)
+        return ret;
 
-	HAL_Delay(10);
-	ret = HAL_I2C_Master_Receive(&hi2c1, (uint16_t)(ZephyrADDR << 1)|0x01, &addata1[2], 2, 50);
-	if(ret != HAL_OK){
-		return ret;
-	}
+    HAL_Delay(2);
+    ret = HAL_I2C_Master_Receive(hi2c, (uint16_t) (ZephyrADDR << 1) | 0x01, &addata1[0], 2, 50);
+    if (ret != HAL_OK)
+        return ret;
 
-	*serialNB = ((addata1[0] & 0xFFFFFFFF) << 24) | ((addata1[1] & 0xFFFFFFFF) << 16) | ((addata1[2] & 0xFFFFFFFF) << 8) | addata1[3];
-	return ret;
+    HAL_Delay(10);
+    ret = HAL_I2C_Master_Receive(hi2c, (uint16_t) (ZephyrADDR << 1) | 0x01, &addata1[2], 2, 50);
+    if (ret != HAL_OK)
+        return ret;
+
+    *serialNB = ((addata1[0] & 0xFF) << 24)
+              | ((addata1[1] & 0xFF) << 16)
+              | ((addata1[2] & 0xFF) << 8)
+              |   addata1[3];
+
+    return ret;
 }
