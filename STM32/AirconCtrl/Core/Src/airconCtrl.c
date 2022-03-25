4401b61b4df89d7b9259fc76d9c5accaf3d385f0
Thomas Pedersen <thomas.jam@copenhagenatomics.com>
2022-03-25 08:36:21 +0100

    IR reader, added interrupthandler

--- STM32/AirconCtrl/Core/Src/airconCtrl.c
+++ STM32/AirconCtrl/Core/Src/airconCtrl.c
@@ -10,6 +10,7 @@
 #include "CAProtocol.h"
 #include "CAProtocolStm.h"
 #include "usb_cdc_fops.h"
+#include "stm32f4xx_hal_tim.h"
 
 static CAProtocolCtx caProto =
 {
@@ -27,29 +28,32 @@ uint32_t tempCode;
 uint8_t bitIndex;
 uint8_t cmd;
 uint8_t cmdli;
+TIM_HandleTypeDef  timerHandel;
 
-void airconCtrlInit()
+void airconCtrlInit(TIM_HandleTypeDef htim1)
 {
+	timerHandel = htim1;
 	initCAProtocol(&caProto, usb_cdc_rx);
-	HAL_TIM_Base_Start(&htim1);
-        __HAL_TIM_SET_COUNTER(&htim1, 0);
+    HAL_TIM_Base_Start(&timerHandel);
+    __HAL_TIM_SET_COUNTER(&timerHandel, 0);
+
 }
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
   if(GPIO_Pin == GPIO_PIN_11)
   {
-    if (__HAL_TIM_GET_COUNTER(&htim1) > 8000)
+    if (__HAL_TIM_GET_COUNTER(&timerHandel) > 8000)
     {
       tempCode = 0;
       bitIndex = 0;
     }
-    else if (__HAL_TIM_GET_COUNTER(&htim1) > 1700)
+    else if (__HAL_TIM_GET_COUNTER(&timerHandel) > 1700)
     {
       tempCode |= (1UL << (31-bitIndex));   // write 1
       bitIndex++;
     }
-    else if (__HAL_TIM_GET_COUNTER(&htim1) > 1000)
+    else if (__HAL_TIM_GET_COUNTER(&timerHandel) > 1000)
     {
       tempCode &= ~(1UL << (31-bitIndex));  // write 0
       bitIndex++;
@@ -60,11 +64,11 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
       cmd = tempCode >> 8; // Second last 8 bits
       if(cmdli == cmd) // Check for errors
       {
-	USBnprintf(tempCode);
+	    USBnprintf("%d", tempCode);
       }
       bitIndex = 0;
     }
-    __HAL_TIM_SET_COUNTER(&htim1, 0);
+    __HAL_TIM_SET_COUNTER(&timerHandel, 0);
   }
 }
 
--- STM32/AirconCtrl/Core/Src/main.c
+++ STM32/AirconCtrl/Core/Src/main.c
@@ -40,6 +40,7 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim1;
 
 /* USER CODE BEGIN PV */
 
@@ -48,6 +49,7 @@
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_TIM1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -86,8 +88,9 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USB_DEVICE_Init();
+  MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
-  airconCtrlInit();
+  airconCtrlInit(&htim1);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -146,6 +149,52 @@ void SystemClock_Config(void)
 }
 
 /**
+  * @brief TIM1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM1_Init(void)
+{
+
+  /* USER CODE BEGIN TIM1_Init 0 */
+
+  /* USER CODE END TIM1_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM1_Init 1 */
+
+  /* USER CODE END TIM1_Init 1 */
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 71;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 65535;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM1_Init 2 */
+
+  /* USER CODE END TIM1_Init 2 */
+
+}
+
+/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
@@ -159,11 +208,25 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PB6 */
+  GPIO_InitStruct.Pin = GPIO_PIN_6;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
   /*Configure GPIO pin : PB7 */
   GPIO_InitStruct.Pin = GPIO_PIN_7;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
 
 }
 
