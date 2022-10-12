9f5d516552feb3126fb1918731986a0263289e0a
matias-cphatomics <matias.tofteby@copenhagenatomics.com>
2022-10-12 14:28:09 +0200

    Update pwm frequency

--- STM32/LightController/Core/Src/main.c
+++ STM32/LightController/Core/Src/main.c
@@ -352,7 +352,7 @@ static void MX_TIM4_Init(void)
 
   /* USER CODE END TIM4_Init 1 */
   htim4.Instance = TIM4;
-  htim4.Init.Prescaler = 63;
+  htim4.Init.Prescaler = 7999;
   htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim4.Init.Period = 255;
   htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
