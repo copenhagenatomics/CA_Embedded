1f31d53451d6b2114e8762b8e43523c958d9950f
matias-cphatomics <72007269+matias-cphatomics@users.noreply.github.com>
2024-04-10 13:21:05 +0200

    UnitTest: Use fakes/stubs in CA_Embedded_Libraries location (#153)
    * Use fakes/stubs in CA_Embedded_Libraries and rm them from CA_Embbeded
    
    * Update workflow to run unittests
    
    * Retrigger workflow
    
    * Update workflow
    
    * Update workflow again...
    
    * 3rd times a charm
    
    * Stopped hoping
    
    * Update python action
    
    * Update actions versions
    
    * Added space to ACBoard.c to invoke workflow
    
    * Update workflow file
    
    * Update unittesting path in workflow
    
    * Divide CMakeLists.txt into subdirectories
    
    * Checking failure procedure
    
    * Force workflow failure on failing test
    
    * Force workflow failure on failing test 2
    
    * Clean tests
    
    * Review changes

--- unit_testing/unitTests.py
+++ unit_testing/unitTests.py
@@ -3,22 +3,42 @@
 import argparse
 import subprocess
 import os
+import sys
+
+def run_tests_in_subdirectory(dir, regex, verbose):
+    subprocess.run("cmake -S . -B build", shell=True, cwd=dir)
+    subprocess.run("cmake --build build", shell=True, cwd=dir)
+
+    run_str = "cd build && ctest"
+    run_str += regex
+    run_str += verbose
+
+    ret = subprocess.run(run_str, shell=True, cwd=dir)
+    return ret.returncode
+        
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run unit tests')
+    parser.add_argument('-D', '--dir', help='Run the CMakeLists.txt in the specified directory')
     parser.add_argument('-R', '--regex', help='Only run the unit tests matching this regex')
     parser.add_argument('-v', '--verbose', action="store_true", help='Maximum output')
     args = parser.parse_args()
 
-    subprocess.run("cmake -S . -B build", shell=True)
-    subprocess.run("cmake --build build", shell=True)
-
-    run_str = "cd build && ctest"
-
+    regex=""
     if(args.regex):
-        run_str += f" -R {args.regex}"
+        regex += f" -R {args.regex}"
 
+    verbose=""
     if(args.verbose):
-        run_str += f" --output-on-failure"
+        verbose += f" --output-on-failure"
 
-    subprocess.run(run_str, shell=True)
+    # Run the tests in the specified directory
+    if (args.dir):
+        sys.exit(run_tests_in_subdirectory(f"{args.dir}", regex, verbose))
+
+    #If the directory is not specified then run all tests from all sub-directories
+    returncodes = []
+    for f in os.scandir(os.getcwd()):
+        if f.is_dir():
+            returncodes.append(run_tests_in_subdirectory(f.name, regex, verbose))
+    sys.exit(any(r != 0 for r in returncodes))
