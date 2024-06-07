34083840cb7123279ec6dad3d818c978e1ea9445
Luke Walker <luke.walker@copenhagenatomics.com>
2024-06-07 10:54:24 +0200

    .IOC firmware update

--- STM32/Temperature/USB_DEVICE/App/usbd_cdc_if.h
+++ STM32/Temperature/USB_DEVICE/App/usbd_cdc_if.h
@@ -48,8 +48,8 @@
   * @{
   */
 /* Define size for the receive and transmit buffer over CDC */
-#define APP_RX_DATA_SIZE  1000
-#define APP_TX_DATA_SIZE  1000
+#define APP_RX_DATA_SIZE  1024
+#define APP_TX_DATA_SIZE  1024
 /* USER CODE BEGIN EXPORTED_DEFINES */
 /* Define size for the receive and transmit buffer over CDC */
 /* It's up to user to redefine and/or remove those define */
