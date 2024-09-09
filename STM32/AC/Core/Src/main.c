fac755c7a820e29443fe967655ea5bc4f8b87b68
matias-cphatomics <72007269+matias-cphatomics@users.noreply.github.com>
2024-09-09 11:20:44 +0200

    Fail test on purpose

--- STM32/AC/Core/Src/main.c
+++ STM32/AC/Core/Src/main.c
@@ -92,9 +92,10 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_ADC1_Init();
   MX_DMA_Init();
   MX_USB_DEVICE_Init();
-  MX_ADC1_Init();
+  
   MX_TIM2_Init();
   MX_WWDG_Init();
   MX_IWDG_Init();
