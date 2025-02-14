eda82040460c664aaca26644959e908e50f05b2f
matias-cphatomics <72007269+matias-cphatomics@users.noreply.github.com>
2025-02-14 09:55:59 +0100

    Lib update + changed test command

--- STM32/LightController/Core/Src/LightController.c
+++ STM32/LightController/Core/Src/LightController.c
@@ -110,9 +110,9 @@ static void LightControllerStatus()
 
 static bool isInputValid(const char *input, int *channel, unsigned int *rgb)
 {
-    // If PARTY command is entered start the colour test
+    // If test command is entered start the colour test
     // If any other input is entered stop it again
-    if (strcmp(input, "PARTY") == 0) 
+    if (strcmp(input, "test") == 0) 
     {
         isInTest = true;
         return true;
