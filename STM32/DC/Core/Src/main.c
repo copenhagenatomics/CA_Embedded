869b4e6bed546e36ed507445693b4b10c91cdc79
matias-cphatomics <72007269+matias-cphatomics@users.noreply.github.com>
2025-02-26 13:52:46 +0100

    Update timer bug

--- STM32/DC/Core/Src/main.c
+++ STM32/DC/Core/Src/main.c
@@ -122,7 +122,7 @@ int main(void)
   HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_3);
   HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
   HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_2);
-  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);
+  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_3);
 
   /* USER CODE END 2 */
 
