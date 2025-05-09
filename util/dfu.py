40ca71100166281dd205a14a9cbe87f3e87f6111
TimotheDodinCA <timothe.dodin@copenhagenatomics.com>
2025-05-09 13:49:31 +0200

    YML update for multi .bin and Current update

--- util/dfu.py
+++ util/dfu.py
@@ -4,7 +4,9 @@ import argparse
 import subprocess
 import os
 from time import sleep
+from elftools.elf.elffile import ELFFile
 import paramiko
+import serial
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Compile/download firmware to board.')
@@ -19,8 +21,8 @@ if __name__ == "__main__":
 
     if args.reset:
         # Make a dummy upload to force the device to reset
-        subprocess.run(f"dfu-util -a 0 -s 0x08000000:leave -U temp.bin", shell=True)
-        subprocess.run(f"rm -rf temp.bin", shell=True)
+        subprocess.run("dfu-util -a 0 -s 0x08000000:leave -U temp.bin", shell=True, check=True)
+        subprocess.run("rm -rf temp.bin", shell=True, check=True)
 
     elif args.board:
         # Change to the correct directory to run make
@@ -36,17 +38,43 @@ if __name__ == "__main__":
 
             subprocess.run(command, shell=True, check=True)
 
+            sector_2_address = 0x08008000
+
+            # Gets the start of the FLASH sector by looking into the .elf file (.text must be the first section)
+            with open(f"build/{args.board}.elf", "rb") as f:
+                elf = ELFFile(f)
+                text_section = elf.get_section_by_name(".text")
+                if text_section:
+                    flash_address = text_section["sh_addr"]
+                else:
+                    raise ValueError(".text section not found")
+
+            # If the FLASH sector starts before SECTOR 2, it means there are no CAL or UPTIME before
+            # It can thus use the entire .bin file
+            if flash_address < sector_2_address:
+                use_seperate_bins = False
+            else:
+                use_seperate_bins = True
+
             if args.port or args.dfu:
                 if args.remote:
                     # Connect over SSH
                     ssh = paramiko.SSHClient()
                     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                    ssh.connect(f"{args.remote}.local", username="pi", password=f"{args.password}")
+                    if os.name == "nt":
+                        ssh.connect(f"{args.remote}", username="pi", password=f"{args.password}", look_for_keys=False, allow_agent=False)
+                    else:
+                        ssh.connect(f"{args.remote}.local", username="pi", password=f"{args.password}")
+
                     ssh.exec_command("tmux kill-server")
 
                     # Copy file over
                     sftp = ssh.open_sftp()
-                    sftp.put(f"build/{args.board}.bin", f"/home/pi/{args.board}.bin")
+                    if use_seperate_bins:
+                        sftp.put(f"build/{args.board}_FLASHISR.bin", f"/home/pi/{args.board}_FLASHISR.bin")
+                        sftp.put(f"build/{args.board}_FLASH.bin", f"/home/pi/{args.board}_FLASH.bin")
+                    else:
+                        sftp.put(f"build/{args.board}.bin", f"/home/pi/{args.board}.bin")
                     sftp.close()
                     
                     # Reset port
@@ -54,23 +82,34 @@ if __name__ == "__main__":
                         ssh.exec_command(f"echo $'DFU\n' > {args.port}")
                         sleep(1.0)
                     
-                    # Download file
-                    stdin, stdout, stderr = ssh.exec_command(f"sudo dfu-util -a 0 -D /home/pi/{args.board}.bin -s 0x08000000:leave")
+                    # Download files
+                    commands = []
+                    if use_seperate_bins:
+                        commands.append(f"sudo dfu-util -a 0 -D /home/pi/{args.board}_FLASHISR.bin -s 0x08000000")
+                        commands.append(f"sudo dfu-util -a 0 -D /home/pi/{args.board}_FLASH.bin -s {hex(flash_address)}:leave")
+                        commands.append(f"sudo rm /home/pi/{args.board}_FLASHISR.bin")
+                        commands.append(f"sudo rm /home/pi/{args.board}_FLASH.bin")
+                    else:
+                        commands.append(f"sudo dfu-util -a 0 -D /home/pi/{args.board}.bin -s 0x08000000:leave")
+                        commands.append(f"rm /home/pi/{args.board}.bin")
+                    
+                    for command in commands:
+                        stdin, stdout, stderr = ssh.exec_command(command)
 
-                    sleep(0.5)
+                        sleep(0.5)
 
-                    # Print status while downloading
-                    while not stdout.channel.exit_status_ready() or stdout.channel.recv_ready():
-                        if stdout.channel.recv_ready():
-                            print(stdout.readline(1), end="")
+                        # Print status while downloading
+                        while not stdout.channel.exit_status_ready() or stdout.channel.recv_ready():
+                            if stdout.channel.recv_ready():
+                                print(stdout.readline(1), end="")
 
-                    if stderr.channel.recv_ready():
-                        for line in iter(stderr.readline, ""):
-                            print(line, end = "")
+                        if stderr.channel.recv_ready():
+                            for line in iter(stderr.readline, ""):
+                                print(line, end = "")
 
-                    stdin.close()
-                    stdout.close()
-                    stderr.close()
+                        stdin.close()
+                        stdout.close()
+                        stderr.close()
 
                     ssh.exec_command("systemctl --user restart tmux.service")
 
@@ -79,10 +118,16 @@ if __name__ == "__main__":
                 else:
                     # Open the port and put the device in DFU mode
                     if args.port:
-                        subprocess.run(f"echo 'DFU\n' > {args.port}", shell=True)
+                        with serial.Serial(str(args.port), 115200, timeout=1) as ser:
+                            ser.write(b'DFU\n')
                         sleep(1.0)
 
-                    subprocess.run(f"dfu-util -a 0 -D build/{args.board}.bin -s 0x08000000:leave", shell=True)
+                    if use_seperate_bins:
+                        subprocess.run(f"dfu-util -a 0 -D build/{args.board}_FLASHISR.bin -s 0x08000000", shell=True, check=True)
+                        subprocess.run(f"dfu-util -a 0 -D build/{args.board}_FLASH.bin -s {hex(flash_address)}:leave", shell=True, check=True)
+                    else:
+                        subprocess.run(f"dfu-util -a 0 -D build/{args.board}.bin -s 0x08000000:leave", shell=True, check=True)
+                        
 
         except Exception as e:
             print(e)
