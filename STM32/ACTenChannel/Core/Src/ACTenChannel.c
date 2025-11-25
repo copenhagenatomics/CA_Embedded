d243936c0e5fffdba883dea20559bf971828065f
Luke Walker <luke.walker@copenhagenatomics.com>
2025-11-25 15:34:59 +0100

    Formatting

--- STM32/ACTenChannel/Core/Src/ACTenChannel.c
+++ STM32/ACTenChannel/Core/Src/ACTenChannel.c
@@ -2,44 +2,44 @@
  * ACTenChannel.c
  */
 
-#include <math.h>
-#include <string.h>
-#include <stdio.h>
 #include <ctype.h>
-#include <stdarg.h>
-#include <stdbool.h>
 #include <float.h>
 #include <inttypes.h>
+#include <math.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
 
-#include "main.h"
-#include "HeatCtrl.h"
-#include "ADCMonitor.h"
-#include "systemInfo.h"
-#include "USBprint.h"
-#include "CAProtocol.h"
-#include "CAProtocolStm.h"
-#include "StmGpio.h"
 #include "ACTenChannel.h"
-#include "pcbversion.h"
+#include "ADCMonitor.h"
+#include "CAProtocol.h"
 #include "CAProtocolACDC.h"
+#include "CAProtocolStm.h"
+#include "HeatCtrl.h"
+#include "StmGpio.h"
+#include "USBprint.h"
+#include "main.h"
+#include "pcbversion.h"
+#include "systemInfo.h"
 
 /***************************************************************************************************
 ** DEFINES
 ***************************************************************************************************/
 
-#define ADC_CHANNELS	        10
-#define ADC_CHANNEL_BUF_SIZE	400
+#define ADC_CHANNELS         10
+#define ADC_CHANNEL_BUF_SIZE 400
 
-#define USB_COMMS_TIMEOUT_MS    5000
+#define USB_COMMS_TIMEOUT_MS 5000
 
 /***************************************************************************************************
 ** PRIVATE TYPEDEFS
 ***************************************************************************************************/
 
 typedef struct ActuationInfo {
-    int pin; // pins 0-9 are interpreted as single ports - pin '-1' is interpreted as all
+    int pin;  // pins 0-9 are interpreted as single ports - pin '-1' is interpreted as all
     int pwmDutyCycle;
-    int timeOn; // time on is in seconds
+    int timeOn;  // time on is in seconds
     bool isInputValid;
 } ActuationInfo;
 
@@ -49,7 +49,7 @@ typedef struct ActuationInfo {
 
 static void CAallOn(bool isOn, int duration);
 static void CAportState(int port, bool state, int percent, int duration);
-static void ACTenChannelInputHandler(const char *input);
+static void ACTenChannelInputHandler(const char* input);
 static void printAcTenChannelStatus();
 static void printAcTenChannelStatusDef();
 static void updateBoardStatus();
@@ -61,25 +61,18 @@ static void updateBoardStatus();
 /* GPIO settings. */
 StmGpio heaterPorts[AC_TEN_CH_NUM_PORTS];
 
-static ACDCProtocolCtx acProto =
-{
-        .allOn = CAallOn,
-        .portState = CAportState
-};
+static ACDCProtocolCtx acProto = {.allOn = CAallOn, .portState = CAportState};
 
-static CAProtocolCtx caProto =
-{
-        .undefined = ACTenChannelInputHandler,
-        .printHeader = CAPrintHeader,
-        .printStatus = printAcTenChannelStatus,
-        .printStatusDef = printAcTenChannelStatusDef,
-        .jumpToBootLoader = HALJumpToBootloader,
-        .calibration = NULL,
-        .calibrationRW = NULL,
-        .logging = NULL,
-        .otpRead = CAotpRead,
-        .otpWrite = NULL
-};
+static CAProtocolCtx caProto = {.undefined        = ACTenChannelInputHandler,
+                                .printHeader      = CAPrintHeader,
+                                .printStatus      = printAcTenChannelStatus,
+                                .printStatusDef   = printAcTenChannelStatusDef,
+                                .jumpToBootLoader = HALJumpToBootloader,
+                                .calibration      = NULL,
+                                .calibrationRW    = NULL,
+                                .logging          = NULL,
+                                .otpRead          = CAotpRead,
+                                .otpWrite         = NULL};
 
 /* Status printout buffer, shared */
 static char buf[600] = {0};
@@ -116,8 +109,8 @@ static void printAcTenChannelStatusDef() {
     CA_SNPRINTF(buf, len, "0x%08" PRIx32 ",Mains not-connected error\r\n", AC_POWER_ERROR_Msk);
     CA_SNPRINTF(buf, len, "0x%08" PRIx32 ",Fan state\r\n", AC_TEN_CH_PORT_x_STATUS_Msk(10));
     for (int i = 0; i < AC_TEN_CH_NUM_PORTS; i++) {
-        CA_SNPRINTF(buf, len, "0x%08" PRIx32 ",Port %d switching state\r\n", 
-            (uint32_t) AC_TEN_CH_PORT_x_STATUS_Msk(i), i);
+        CA_SNPRINTF(buf, len, "0x%08" PRIx32 ",Port %d switching state\r\n",
+                    (uint32_t)AC_TEN_CH_PORT_x_STATUS_Msk(i), i);
     }
 
     writeUSB(buf, len);
@@ -128,17 +121,18 @@ static void printAcTenChannelStatusDef() {
 **
 ** @param input The user input string
 */
-static void ACTenChannelInputHandler(const char *input) { ACDCInputHandler(&acProto, input); }
+static void ACTenChannelInputHandler(const char* input) {
+    ACDCInputHandler(&acProto, input);
+}
 
-static void GpioInit()
-{
-    static GPIO_TypeDef *const pinsBlk[] = { CTRL_0_GPIO_Port, CTRL_1_GPIO_Port, CTRL_2_GPIO_Port, CTRL_3_GPIO_Port, CTRL_4_GPIO_Port,
-                                             CTRL_5_GPIO_Port, CTRL_6_GPIO_Port, CTRL_7_GPIO_Port, CTRL_8_GPIO_Port, CTRL_9_GPIO_Port };
-    static const uint16_t pins[] = { CTRL_0_Pin, CTRL_1_Pin, CTRL_2_Pin, CTRL_3_Pin, CTRL_4_Pin,
-                                     CTRL_5_Pin, CTRL_6_Pin, CTRL_7_Pin, CTRL_8_Pin, CTRL_9_Pin };
+static void GpioInit() {
+    static GPIO_TypeDef* const pinsBlk[] = {
+        CTRL_0_GPIO_Port, CTRL_1_GPIO_Port, CTRL_2_GPIO_Port, CTRL_3_GPIO_Port, CTRL_4_GPIO_Port,
+        CTRL_5_GPIO_Port, CTRL_6_GPIO_Port, CTRL_7_GPIO_Port, CTRL_8_GPIO_Port, CTRL_9_GPIO_Port};
+    static const uint16_t pins[] = {CTRL_0_Pin, CTRL_1_Pin, CTRL_2_Pin, CTRL_3_Pin, CTRL_4_Pin,
+                                    CTRL_5_Pin, CTRL_6_Pin, CTRL_7_Pin, CTRL_8_Pin, CTRL_9_Pin};
 
-    for (int i = 0; i < AC_TEN_CH_NUM_PORTS; i++)
-    {
+    for (int i = 0; i < AC_TEN_CH_NUM_PORTS; i++) {
         stmGpioInit(&heaterPorts[i], pinsBlk[i], pins[i], STM_GPIO_OUTPUT);
         heatCtrlAdd(&heaterPorts[i]);
     }
@@ -146,31 +140,26 @@ static void GpioInit()
     stmGpioInit(&powerStatus, POWERSTATUS_GPIO_Port, POWERSTATUS_Pin, STM_GPIO_INPUT);
 }
 
-static double ADCtoCurrent(double adc_val)
-{
+static double ADCtoCurrent(double adc_val) {
     static const float CURRENT_SCALAR = 0.013138;
-    static const float CURRENT_BIAS = -0.01;
+    static const float CURRENT_BIAS   = -0.01;
 
     return CURRENT_SCALAR * adc_val + CURRENT_BIAS;
 }
 
-static void printCurrentArray(int16_t *pData, int noOfChannels, int noOfSamples)
-{
+static void printCurrentArray(int16_t* pData, int noOfChannels, int noOfSamples) {
     // Make calibration static since this should be done only once.
     static bool isCalibrationDone = false;
     static int16_t current_calibration[ADC_CHANNELS];
     static uint32_t port_close_time = 0;
 
-    /* If the USB port is not open, no messages should be printed. Also if the USB port has been 
+    /* If the USB port is not open, no messages should be printed. Also if the USB port has been
     ** closed for more than a timeout, everything should be turned off as a safety measure */
-    if (!isUsbPortOpen()) 
-    {
-        if (port_close_time == 0)
-        {
+    if (!isUsbPortOpen()) {
+        if (port_close_time == 0) {
             port_close_time = HAL_GetTick();
         }
-        else if((HAL_GetTick() - port_close_time) >= USB_COMMS_TIMEOUT_MS)
-        {
+        else if ((HAL_GetTick() - port_close_time) >= USB_COMMS_TIMEOUT_MS) {
             allOff();
         }
         return;
@@ -178,16 +167,13 @@ static void printCurrentArray(int16_t *pData, int noOfChannels, int noOfSamples)
     port_close_time = 0;
 
     /* If the version is incorrect, there is no point printing data or doing maths */
-    if (bsGetField(BS_VERSION_ERROR_Msk))
-    {
+    if (bsGetField(BS_VERSION_ERROR_Msk)) {
         USBnprintf("0x%08" PRIx32, bsGetStatus());
         return;
     }
 
-    if (!isCalibrationDone)
-    {
-        for (int i = 0; i < noOfChannels; i++)
-        {
+    if (!isCalibrationDone) {
+        for (int i = 0; i < noOfChannels; i++) {
             // finding the average of each channel array to subtract from the readings
             current_calibration[i] = -ADCMean(pData, i);
         }
@@ -195,8 +181,7 @@ static void printCurrentArray(int16_t *pData, int noOfChannels, int noOfSamples)
     }
 
     // Set bias for each ADC channel.
-    for (int i = 0; i < noOfChannels; i++)
-    {
+    for (int i = 0; i < noOfChannels; i++) {
         ADCSetOffset(pData, current_calibration[i], i);
     }
 
@@ -211,81 +196,64 @@ static void printCurrentArray(int16_t *pData, int noOfChannels, int noOfSamples)
 /*!
 ** @brief Calls appropriate backend function based on inputs
 **
-** Depending on the inputs (which are received from communication link), chooses the appropriate 
+** Depending on the inputs (which are received from communication link), chooses the appropriate
 ** backend function and calls it.
 */
-static void actuatePins(ActuationInfo actuationInfo)
-{
+static void actuatePins(ActuationInfo actuationInfo) {
     /* All pins functions */
-    if (actuationInfo.pin == -1)
-    {
-        if (actuationInfo.pwmDutyCycle == 0)
-        {
+    if (actuationInfo.pin == -1) {
+        if (actuationInfo.pwmDutyCycle == 0) {
             // all off (pin == -1 means all pins)
             allOff();
         }
-        else if (actuationInfo.pwmDutyCycle == 100)
-        {
+        else if (actuationInfo.pwmDutyCycle == 100) {
             // all on (pin == -1 means all pins)
             allOn(actuationInfo.timeOn);
         }
         /* It doesn't really make sense to allow setting all pins to the same PWM */
-    } 
-    else
-    {
-        if (actuationInfo.pwmDutyCycle == 0)
-        {
+    }
+    else {
+        if (actuationInfo.pwmDutyCycle == 0) {
             // pX off
             turnOffPin(actuationInfo.pin);
         }
-        else if (actuationInfo.pwmDutyCycle == 100)
-        {
+        else if (actuationInfo.pwmDutyCycle == 100) {
             // pX on YY
             turnOnPin(actuationInfo.pin, actuationInfo.timeOn);
         }
-        else
-        {
+        else {
             // pX on ZZZ%
             setPWMPin(actuationInfo.pin, actuationInfo.pwmDutyCycle, actuationInfo.timeOn);
         }
     }
 }
 
-static void CAallOn(bool isOn, int duration)
-{
-    if (isOn)
-    {
-        if(duration <= 0) 
-        {
+static void CAallOn(bool isOn, int duration) {
+    if (isOn) {
+        if (duration <= 0) {
             char buf[20] = {0};
             snprintf(buf, 20, "all on %d", duration);
             HALundefined(buf);
         }
-        else
-        {
-            allOn(1000*duration);
+        else {
+            allOn(1000 * duration);
         }
     }
-    else
-    {
+    else {
         allOff();
     }
 }
 
-static void CAportState(int port, bool state, int percent, int duration)
-{
-    if((duration <= 0) && (percent != 0)) 
-    {
+static void CAportState(int port, bool state, int percent, int duration) {
+    if ((duration <= 0) && (percent != 0)) {
         char buf[20] = {0};
         snprintf(buf, 20, "p%d on %d", port, duration);
         HALundefined(buf);
     }
-    else 
-    {
-        actuatePins((ActuationInfo) { port - 1, percent, 1000*duration, true });
+    else {
+        actuatePins((ActuationInfo){port - 1, percent, 1000 * duration, true});
     }
 }
-
 
 /*!
 ** @brief Updates the global error/status object.
@@ -296,15 +264,16 @@ static void CAportState(int port, bool state, int percent, int duration)
 static void updateBoardStatus() {
     static int const FILTER_LEN = 1000;
 
-    for(int i = 0; i < AC_TEN_CH_NUM_PORTS; i++)
-    {
-        stmGetGpio(heaterPorts[i]) ? bsSetField(AC_TEN_CH_PORT_x_STATUS_Msk(i)) : bsClearField(AC_TEN_CH_PORT_x_STATUS_Msk(i));
+    for (int i = 0; i < AC_TEN_CH_NUM_PORTS; i++) {
+        stmGetGpio(heaterPorts[i]) ? bsSetField(AC_TEN_CH_PORT_x_STATUS_Msk(i))
+                                   : bsClearField(AC_TEN_CH_PORT_x_STATUS_Msk(i));
     }
 
     /* Heavily averaged signal of the last 1000 samples to smooth out large dips
-    ** The gpio input is not supposed to change value generally so should be fine to use a large filter
+    ** The gpio input is not supposed to change value generally so should be fine to use a large
+    * filter
     ** Note: When power is toggled the status code changes within one print cycle */
-    isMainsConnected += (stmGetGpio(powerStatus) - isMainsConnected)/FILTER_LEN;
+    isMainsConnected += (stmGetGpio(powerStatus) - isMainsConnected) / FILTER_LEN;
     (isMainsConnected >= 0.5) ? bsClearField(AC_POWER_ERROR_Msk) : bsSetError(AC_POWER_ERROR_Msk);
 
     /* Clear the error mask if there are no error bits set any more. This logic could be done when
@@ -321,10 +290,10 @@ static void updateBoardStatus() {
 **
 ** Checks the hardware matches this FW version, starts the USB communication and starts the ADC.
 ** Printing is synchronised with ADC, so it must be started in order to print anything over the USB
-** link 
+** link
 */
-void ACTenChannelInit(ADC_HandleTypeDef *hadc, TIM_HandleTypeDef *htim,
-                      TIM_HandleTypeDef *hDS18B20tim) {
+void ACTenChannelInit(ADC_HandleTypeDef* hadc, TIM_HandleTypeDef* htim,
+                      TIM_HandleTypeDef* hDS18B20tim) {
     // Always allow for DFU also if programmed on non-matching board or PCB version.
     initCAProtocol(&caProto, usbRx);
 
@@ -344,13 +313,13 @@ void ACTenChannelInit(ADC_HandleTypeDef *hadc, TIM_HandleTypeDef *htim,
 
 /*!
 ** @brief Loop function called repeatedly throughout
-** 
+**
 ** * Responds to user input
-** * Checks for ADC buffer switches (the ADC sample rate is synchronised with USB print rate - the 
+** * Checks for ADC buffer switches (the ADC sample rate is synchronised with USB print rate - the
 **   USB print rate should be 10 Hz, so every 400 ADC samples the buffer switches).
 ** * PWMs the heaters as per user input
 */
-void ACTenChannelLoop(const char *bootMsg) {
+void ACTenChannelLoop(const char* bootMsg) {
     CAhandleUserInputs(&caProto, bootMsg);
 
     if (!bsGetField(BS_VERSION_ERROR_Msk)) {
