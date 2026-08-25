addacabd22987371ec979a456085b335dfa1f26e
TimotheDodinCA <timothe.dodin@copenhagenatomics.com>
2026-08-25 09:32:22 +0200

    PCB V2.0

--- STM32/Current/Core/Inc/pcbversion.h
+++ STM32/Current/Core/Inc/pcbversion.h
@@ -1,8 +1,8 @@
 #ifndef PCB_VERSION_H_
 #define PCB_VERSION_H_ 
 
-#define LATEST_MAJOR    1
-#define LATEST_MINOR    9
+#define LATEST_MAJOR    2
+#define LATEST_MINOR    0
 #define BREAKING_MAJOR  1
 #define BREAKING_MINOR  9
 
